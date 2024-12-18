0381ed78eb48105781e2a2644d66b88f814b76e4
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-18 13:48:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 //your JS code here. If required.
 const input  = document.getElementsByClassName("code")
 const div1 = document.getElementsByClassName("code-container")
-let i = 0;
-div1[0].addEventListener("keyup", (e)=>{
-	// console.log(e.target)
-	if(e.key=="Backspace"){
-		i--
-		input[i].focus() 
-	}else{
-		i++
-		input[i].focus()
-	}
-})
+
+// div1[0].addEventListener("keypress", (e)=>{
+// 	// console.log(e.target)
+// 	if(e.key=="Backspace"){
+// 		e.nextElementSibling.focus() 
+// 	}else{
+// 		e.previousElementSibling.focus()
+// 	} 
+// })
+const inputs  = document.getElementsByClassName("code")
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("keyup", function(e) {
+        if (e.target.value) {
+            if (e.target.nextElementSibling) {
+                e.target.nextElementSibling.focus();
+            }
+        } else if (e.key === "Backspace") {
+            if (e.target.previousElementSibling) {
+                e.target.previousElementSibling.focus();
+            }
+        }
+    });
+}
 
 
