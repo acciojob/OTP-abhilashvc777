581fe4c14dd5f1eb8b75304011eb6308665c3a49
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-18 13:25:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const input  = document.getElementsByClassName("code")
+const div1 = document.getElementsByClassName("code-container")
+let i = 0;
+div1[0].addEventListener("keyup", (e)=>{
+	// console.log(e.target)
+	if(e.key=="Backspace"){
+		i--
+		input[i].focus() 
+	}else{
+		i++
+		input[i].focus()
+	}
+})
+
+
